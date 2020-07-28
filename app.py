df354d8c6179918759e13fa035bdd15b0dbb088b
Tran Chi Quang <chiquang1498@gmail.com>
2020-07-28 19:44:50 +0700

    s

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from flask import Flask, request, render_template, jsonify
 
-# from Service import *
-# import pandas as pd
+from Service import *
+import pandas as pd
 
 import threading
 # implementFaceDetect(model,classifier_model,vgg_face)
 from flask import Flask, request, render_template
-# model_fd = load_model_face_detector()
-# classify_model = loaded_model
+model_fd = load_model_face_detector()
+classify_model = loaded_model
 app = Flask(__name__)
-# pretrain_model = load_Pretrain_model()
-# @app.route('/upload', methods=['POST'])
-# def uploadFile():
-#     global person_rep
-#     # person_rep  = load_person_rep(pathPerson_Rep)
-#     file = request.files.get('file')
-#     if file and file.filename != '':
-#         file.save("DataClient/"+file.filename)
-#         listJSON = face_detector_by_image("DataClient/"+file.filename,model_fd,classify_model,pretrain_model)
-#     # print((jsonify(listJSON)))
-#     return pd.Series(listJSON).to_json(orient='values')
+pretrain_model = load_Pretrain_model()
+@app.route('/upload', methods=['POST'])
+def uploadFile():
+    global person_rep
+    # person_rep  = load_person_rep(pathPerson_Rep)
+    file = request.files.get('file')
+    if file and file.filename != '':
+        file.save("DataClient/"+file.filename)
+        listJSON = face_detector_by_image("DataClient/"+file.filename,model_fd,classify_model,pretrain_model)
+    # print((jsonify(listJSON)))
+    return pd.Series(listJSON).to_json(orient='values')
 
 @app.route('/')
 def homePage():
